f4178ba31b93c976ed26b5248a2039c800bf5ac6
deepkolos <deepkolos@qq.com>
2021-01-31 18:54:18 +0800

    fix: toEnvMap import

--- tools/toEnvMap.js
+++ tools/toEnvMap.js
@@ -1,3 +1,15 @@
+import {
+  NearestFilter,
+  UnsignedByteType,
+  RGBEFormat,
+  RGBEEncoding,
+  CubeUVReflectionMapping,
+} from '../build/three.module';
+
+/**
+ * 设置通过TextureLoad加载从PMREMGenerator生成纹理导出成PNG
+ * @param {Texture} texture
+ */
 export function toEnvMap(texture) {
   texture.generateMipmaps = false;
   texture.magFilter = NearestFilter;
