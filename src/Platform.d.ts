9325f7bc173a9e253fdd37c189641162c2cf3929
deepkolos <deepkolos@qq.com>
2021-02-18 16:13:59 +0800

    fix: update Platform.d.ts

--- src/Platform.d.ts
+++ src/Platform.d.ts
@@ -5,13 +5,16 @@ export class Platform {
 }
 
 export const PLATFORM: Platform;
-export let $atob: any;
-export let $window: any;
-export let $document: any;
-export let $XMLHttpRequest: any;
-export let $OffscreenCanvas: any;
-export let $HTMLCanvasElement: any;
-export let $createImageBitmap: any;
-export let $requestAnimationFrame: any;
-export let $cancelAnimationFrame: any;
-
+export let $URL: URL;
+export let $atob: Window['atob'];
+export let $Blob: Blob;
+export let $window: Window;
+export let $document: Document;
+export let $DOMParser: DOMParser;
+export let $TextDecoder: TextDecoder;
+export let $XMLHttpRequest: XMLHttpRequest;
+export let $OffscreenCanvas: OffscreenCanvas;
+export let $HTMLCanvasElement: HTMLCanvasElement;
+export let $createImageBitmap: Window['createImageBitmap'];
+export let $cancelAnimationFrame: Window['cancelAnimationFrame'];
+export let $requestAnimationFrame: Window['requestAnimationFrame'];
