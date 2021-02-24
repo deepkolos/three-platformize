441865bb5ecd733fa2e91dc1f92333135ffbe442
deepkolos <deepkolos@qq.com>
2021-02-24 22:25:45 +0800

    test: add memory test

---
+++ config/rollup.config.tests.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    input: `tests/memory.js`,
+    output: {
+      format: 'esm',
+      file: `tests/memory.bundle.js`,
+    },
+  },
+];
---
+++ tests/memory.js
@@ -0,0 +1,49 @@
+import * as THREE from '../build/three.module.js';
+import { BrowserPlatform } from '../src/BrowserPlatform/index.js';
+import { GLTFLoader } from '../examples/jsm/loaders/GLTFLoader.js';
+import { MeshoptDecoder } from '../examples/jsm/libs/meshopt_decoder.module.js';
+
+async function main() {
+  THREE.PLATFORM.set(new BrowserPlatform());
+
+  let frame;
+  const renderer = new THREE.WebGL1Renderer({ alpha: true });
+  const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+  const loader = new GLTFLoader();
+  const scene = new THREE.Scene();
+
+  renderer.setSize(innerWidth, innerHeight);
+  renderer.setPixelRatio(devicePixelRatio);
+  loader.setMeshoptDecoder(MeshoptDecoder);
+
+  const gltf = await loader.loadAsync('./WaterBottle-EXT_meshopt_compression.glb');
+  scene.add(gltf.scene);
+  scene.add(new THREE.AmbientLight(0xffffff, 1.0));
+  scene.add(new THREE.DirectionalLight(0xffffff, 1.0));
+
+  camera.position.set(0, 0.1, 0.2);
+  camera.lookAt(0, 0, 0);
+
+  const render = () => {
+    frame = requestAnimationFrame(render);
+    renderer.render(scene, camera);
+  };
+
+  render();
+  document.body.appendChild(renderer.domElement);
+
+  setTimeout(() => {
+    // dispose
+    cancelAnimationFrame(frame);
+    document.body.removeChild(renderer.domElement);
+    THREE.PLATFORM.dispose();
+    renderer?.dispose();
+    // camera?.dispose();
+    // loader?.dispose();
+    // scene?.dispose();
+    // gltf?.dispose();
+    main();
+  }, 500);
+}
+
+main();
