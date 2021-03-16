319a80a5d55e85323e976c382e383ffed36f7a74
deepkolos <deepkolos@qq.com>
2021-03-16 17:32:28 +0800

    fix: replace arraybuffer to base64 lib

--- src/libs/base64-arraybuffer.js
+++ src/libs/base64-arraybuffer.js
@@ -14,23 +14,76 @@ for (var i = 0; i < chars.length; i++) {
   lookup[chars.charCodeAt(i)] = i;
 }
 
-export function encode(arraybuffer) {
-  var bytes = new Uint8Array(arraybuffer),
-    i,
-    len = bytes.length,
-    base64 = '';
+// 有点慢
+// export function encode(arraybuffer) {
+//   var bytes = new Uint8Array(arraybuffer),
+//     i,
+//     len = bytes.length,
+//     base64 = '';
 
-  for (i = 0; i < len; i += 3) {
-    base64 += chars[bytes[i] >> 2];
-    base64 += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
-    base64 += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
-    base64 += chars[bytes[i + 2] & 63];
+//   for (i = 0; i < len; i += 3) {
+//     base64 += chars[bytes[i] >> 2];
+//     base64 += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
+//     base64 += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
+//     base64 += chars[bytes[i + 2] & 63];
+//   }
+
+//   if (len % 3 === 2) {
+//     base64 = base64.substring(0, base64.length - 1) + '=';
+//   } else if (len % 3 === 1) {
+//     base64 = base64.substring(0, base64.length - 2) + '==';
+//   }
+
+//   return base64;
+// }
+
+// 快一点
+export function encode(arrayBuffer) {
+  var base64 = '';
+
+  var bytes = new Uint8Array(arrayBuffer);
+  var byteLength = bytes.byteLength;
+  var byteRemainder = byteLength % 3;
+  var mainLength = byteLength - byteRemainder;
+
+  var a, b, c, d;
+  var chunk;
+
+  // Main loop deals with bytes in chunks of 3
+  for (var i = 0; i < mainLength; i = i + 3) {
+    // Combine the three bytes into a single integer
+    chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
+
+    // Use bitmasks to extract 6-bit segments from the triplet
+    a = (chunk & 16515072) >> 18; // 16515072 = (2^6 - 1) << 18
+    b = (chunk & 258048) >> 12; // 258048   = (2^6 - 1) << 12
+    c = (chunk & 4032) >> 6; // 4032     = (2^6 - 1) << 6
+    d = chunk & 63; // 63       = 2^6 - 1
+
+    // Convert the raw binary segments to the appropriate ASCII encoding
+    base64 += chars[a] + chars[b] + chars[c] + chars[d];
   }
 
-  if (len % 3 === 2) {
-    base64 = base64.substring(0, base64.length - 1) + '=';
-  } else if (len % 3 === 1) {
-    base64 = base64.substring(0, base64.length - 2) + '==';
+  // Deal with the remaining bytes and padding
+  if (byteRemainder == 1) {
+    chunk = bytes[mainLength];
+
+    a = (chunk & 252) >> 2; // 252 = (2^6 - 1) << 2
+
+    // Set the 4 least significant bits to zero
+    b = (chunk & 3) << 4; // 3   = 2^2 - 1
+
+    base64 += chars[a] + chars[b] + '==';
+  } else if (byteRemainder == 2) {
+    chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
+
+    a = (chunk & 64512) >> 10; // 64512 = (2^6 - 1) << 10
+    b = (chunk & 1008) >> 4; // 1008  = (2^6 - 1) << 4
+
+    // Set the 2 least significant bits to zero
+    c = (chunk & 15) << 2; // 15    = 2^4 - 1
+
+    base64 += chars[a] + chars[b] + chars[c] + '=';
   }
 
   return base64;
