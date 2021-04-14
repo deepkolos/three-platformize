a4cc3bb1a757e3a30f2c7d398f35afdfe6644d60
deepkolos <deepkolos@qq.com>
2021-04-14 18:34:27 +0800

    fix: browserPlatform OffscreenCanvas ReferenceError

--- src/BrowserPlatform/index.js
+++ src/BrowserPlatform/index.js
@@ -1,15 +1,6 @@
 export class BrowserPlatform {
   getGlobals() {
-    return {
-      atob,
-      Blob,
-      window,
-      document,
-      XMLHttpRequest,
-      OffscreenCanvas,
-      HTMLCanvasElement,
-      createImageBitmap,
-    };
+    return window;
   }
 
   dispose() {}
